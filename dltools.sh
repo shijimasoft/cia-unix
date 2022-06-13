f5261315ba6e8353257e2f24ab3cb5c140493dd0
shijimasoft <shijimasoft@gmail.com>
2022-06-13 14:27:06 +0200

    update dependencies

--- dltools.sh
+++ dltools.sh
@@ -6,31 +6,31 @@ NORMAL=$(tput sgr0)
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     echo " * Downloading ${BOLD}ctrtool${NORMAL}"
-    wget https://github.com/3DSGuy/Project_CTR/releases/download/ctrtool-v0.7/ctrtool-v0.7-macos_x86_64.zip -q
+    wget https://github.com/3DSGuy/Project_CTR/releases/download/ctrtool-v1.1.0/ctrtool-v1.1.0-macos_x86_64.zip -q
     echo " * Extracting ${BOLD}ctrtool${NORMAL}"
-    unzip -qq ctrtool-v0.7-macos_x86_64.zip -d ctrtool-v0.7-macos_x86_64
-    mv ctrtool-v0.7-macos_x86_64/ctrtool "$SCRIPT_DIR/ctrtool"
+    unzip -qq ctrtool-v1.1.0-macos_x86_64.zip -d ctrtool-v1.1.0-macos_x86_64
+    mv ctrtool-v1.1.0-macos_x86_64/ctrtool "$SCRIPT_DIR/ctrtool"
     echo " * Downloading ${BOLD}makerom${NORMAL}"
-    wget https://github.com/3DSGuy/Project_CTR/releases/download/makerom-v0.18/makerom-v0.18-macos_x86_64.zip -q
+    wget https://github.com/3DSGuy/Project_CTR/releases/download/makerom-v0.18.3/makerom-v0.18.3-macos_x86_64.zip -q
     echo " * Extracting ${BOLD}makerom${NORMAL}"
-    unzip -qq makerom-v0.18-macos_x86_64.zip -d makerom-v0.18-macos_x86_64
-    mv makerom-v0.18-macos_x86_64/makerom "$SCRIPT_DIR/makerom"
+    unzip -qq makerom-v0.18.3-macos_x86_64.zip -d makerom-v0.18.3-macos_x86_64
+    mv makerom-v0.18.3-macos_x86_64/makerom "$SCRIPT_DIR/makerom"
 
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     echo " * Downloading ${BOLD}ctrtool${NORMAL}"
-    wget https://github.com/3DSGuy/Project_CTR/releases/download/ctrtool-v0.7/ctrtool-v0.7-ubuntu_x86_64.zip -q
+    wget https://github.com/3DSGuy/Project_CTR/releases/download/ctrtool-v1.1.0/ctrtool-v1.1.0-ubuntu_x86_64.zip -q
     echo " * Extracting ${BOLD}ctrtool${NORMAL}"
-    unzip -qq ctrtool-v0.7-ubuntu_x86_64.zip -d ctrtool-v0.7-ubuntu_x86_64
-    mv ctrtool-v0.7-ubuntu_x86_64/ctrtool "$SCRIPT_DIR/ctrtool"
+    unzip -qq ctrtool-v1.1.0-ubuntu_x86_64.zip -d ctrtool-v1.1.0-ubuntu_x86_64
+    mv ctrtool-v1.1.0-ubuntu_x86_64/ctrtool "$SCRIPT_DIR/ctrtool"
     echo " * Downloading ${BOLD}makerom${NORMAL}"
-    wget https://github.com/3DSGuy/Project_CTR/releases/download/makerom-v0.18/makerom-v0.18-ubuntu_x86_64.zip -q
+    wget https://github.com/3DSGuy/Project_CTR/releases/download/makerom-v0.18.3/makerom-v0.18.3-ubuntu_x86_64.zip -q
     echo " * Extracting ${BOLD}makerom${NORMAL}"
-    unzip -qq makerom-v0.18-ubuntu_x86_64.zip -d makerom-v0.18-ubuntu_x86_64
-    mv makerom-v0.18-ubuntu_x86_64/makerom "$SCRIPT_DIR/makerom"
+    unzip -qq makerom-v0.18.3-ubuntu_x86_64.zip -d makerom-v0.18.3-ubuntu_x86_64
+    mv makerom-v0.18.3-ubuntu_x86_64/makerom "$SCRIPT_DIR/makerom"
 fi
 
 echo " * Cleaning up"
-rm -rf ctrtool-v0.7-*
-rm -rf makerom-v0.18-*
+rm -rf ctrtool-v1.1.0-*
+rm -rf makerom-v0.18.3-*
 
 chmod +x ctrtool makerom
